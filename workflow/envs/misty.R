6027ec4d1f939350738435eb18ee9dbc24d6d0ec
demian1 <robin.fallegger@googlemail.com>
2023-05-30 11:11:08 +0200

    updated env for changes in tidyverse

--- workflow/envs/misty.R
+++ workflow/envs/misty.R
@@ -1,2 +1,2 @@
 remotes::install_version('deldir', version = '1.0-6', repos = "http://cran.us.r-project.org" , upgrade ='never')
-remotes::install_github("saezlab/mistyR", ref = 'v1.4.0', upgrade ='never')
+remotes::install_github("saezlab/mistyR", ref = '65eb11a4229a6e2b933603cd81de59b3316efe39', upgrade ='never')
